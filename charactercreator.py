bb9d37a92782dd9560172ce1056981c8c9a1f705
KiYuu39 <k.bassett.t@gmail.com>
2024-11-03 17:49:26 -0600

    wrote basic test class

--- charactercreator.py
+++ charactercreator.py
@@ -4,17 +4,30 @@ class TestClass(): #using fighter stats for test class
 
     def __init__(self, level=1, proficiencyBonus=2, numFightingStyles=1):
         self.level = level
-        self.profiencyBonus = proficiencyBonus
-        self.fightingStyles = numFightingStyles
+        self.proficiencyBonus = proficiencyBonus
+        self.numFightingStyles = numFightingStyles
 
         self.archetype = ""
-        
+
+    def setValues(self):
+        self.level = input("Level: ")
+        self.archetype = input("Archetype: ")
+
+    def printValues(self):
+        print(self.level)
+        print(self.proficiencyBonus)
+        print(self.numFightingStyles)
+        print(self.archetype)
+
 
 
 
 
 def main():
-    print("test")
+    test = TestClass()
+    test.setValues()
+    test.printValues()
+
 
 if __name__ == "__main__":
     main()
