982b65e974c183a7717a4b7b9402b562395a3dac
KiYuu39 <k.bassett.t@gmail.com>
2024-11-03 17:44:30 -0600

    began writing test class

---
+++ charactercreator.py
@@ -0,0 +1,20 @@
+import random
+
+class TestClass(): #using fighter stats for test class
+
+    def __init__(self, level=1, proficiencyBonus=2, numFightingStyles=1):
+        self.level = level
+        self.profiencyBonus = proficiencyBonus
+        self.fightingStyles = numFightingStyles
+
+        self.archetype = ""
+        
+
+
+
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()
