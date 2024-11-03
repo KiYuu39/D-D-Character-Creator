f575a0c0272820d7d9090fecc2a6eb9cd48a7134
KiYuu39 <k.bassett.t@gmail.com>
2024-11-03 17:59:35 -0600

    wrote test cases in test class

--- charactercreator.py
+++ charactercreator.py
@@ -6,12 +6,32 @@ class TestClass(): #using fighter stats for test class
         self.level = level
         self.proficiencyBonus = proficiencyBonus
         self.numFightingStyles = numFightingStyles
+        self.fightingStyle1 = "none"
 
-        self.archetype = ""
+        self.archetype = "none"
 
     def setValues(self):
-        self.level = input("Level: ")
-        self.archetype = input("Archetype: ")
+        valid = False
+        while not valid:
+            self.level = int(input("Level: "))  #get level (must be 1-3)
+            if self.level == 3: #if level = 3, allow break loop & ask for archetype
+                valid = True
+                self.archetype = input("Archetype: ")
+            elif self.level>3: #if level>3, loop again
+                print("Please enter a number 1-3.")
+            else: #allow break loop
+                valid = True
+        
+        count = self.numFightingStyles
+        while count>0:
+            answer = input(f"You have {count} fighting styles to choose. To get information on fighting styles, type Q. Otherwise, enter fighting style: ")
+            if answer == 'Q' or answer == 'q':
+                print("information")
+            else:
+                self.fightingStyle1 = answer
+                count-=1
+            
+            
 
     def printValues(self):
         print(self.level)
