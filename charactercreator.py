d2a812c8ea075c885acd72742ee9ea36efee5622
KiYuu39 <k.bassett.t@gmail.com>
2024-11-05 13:12:23 -0600

    began writing barbarian class

--- charactercreator.py
+++ charactercreator.py
@@ -1,6 +1,6 @@
 import random
 
-class TestClass(): #using fighter stats for test class
+class Fighter(): #fighter class
 
     def __init__(self, level=1, proficiencyBonus=2, numFightingStyles=1):
         self.level = level
@@ -16,7 +16,9 @@ class TestClass(): #using fighter stats for test class
             self.level = int(input("Level: "))  #get level (must be 1-3)
             if self.level == 3: #if level = 3, allow break loop & ask for archetype
                 valid = True
-                self.archetype = input("Archetype: ")
+                print("Archetypes: ") #aka subclass
+                print("\t- Battle Master\n\t- Champion\n\t- Eldritch Knight")
+                self.archetype = input("Enter archetype: ")
             elif self.level>3: #if level>3, loop again
                 print("Please enter a number 1-3.")
             else: #allow break loop
@@ -26,7 +28,9 @@ class TestClass(): #using fighter stats for test class
         while count>0:
             answer = input(f"You have {count} fighting styles to choose. To get information on fighting styles, type Q. Otherwise, enter fighting style: ")
             if answer == 'Q' or answer == 'q':
-                print("information")
+                print("Fighting Styles:")
+                print("\t- archery\n\t- defense\n\t- dueling\n\t- great weapon fighting\n\t- protection\n\t- two-weapon fighting")
+
             else:
                 self.fightingStyle1 = answer
                 count-=1
@@ -39,12 +43,65 @@ class TestClass(): #using fighter stats for test class
         print(self.numFightingStyles)
         print(self.archetype)
 
+    def fileInputLevelClassSubclass(self):
+        print("")
+        #level, class and subclass
+
+    def fileInputFeatures(self):
+        print("")
+        #class features
+        #in this case, action surge (if any), class-specific abilities, etc.
+
+class Barbarian(): #barbarian class UNFINISHED
+
+    def __init__(self, level=1, proficiencyBonus=2, rageDamageBonus=2):
+        self.level = level
+        self.proficiencyBonus = proficiencyBonus
+        self.rageDamageBonus = rageDamageBonus
+        self.rageCount = 2
+        
+
+        self.subclass = "none"
+
+    def setValues(self):
+        valid = False
+        while not valid:
+            self.level = int(input("Level: "))  #get level (must be 1-3)
+            if self.level == 3: #if level = 3, allow break loop & ask for subclass
+                valid = True
+                print("Primal paths: ") #aka subclass
+                print("\t- Berserker\n\t- Totem Warrior")
+                self.archetype = input("Enter primal path: ")
+            elif self.level>3: #if level>3, loop again
+                print("Please enter a number 1-3.")
+            else: #allow break loop
+                valid = True
+        
+        if self.level>=3 and self.level<6:  #ragecount adjust for lv3+
+            self.rageCount = 3
+            
+            
+
+    def printValues(self):
+        print(self.level)
+        print(self.proficiencyBonus)
+        print(self.numFightingStyles)
+        print(self.archetype)
+
+    def fileInputLevelClassSubclass(self):
+        print("")
+        #level, class and subclass
+
+    def fileInputFeatures(self):
+        print("")
+        #class features
+        #in this case, action surge (if any), class-specific abilities, etc.
 
 
 
 
 def main():
-    test = TestClass()
+    test = Fighter()
     test.setValues()
     test.printValues()
 
